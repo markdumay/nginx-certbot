1f40c9009af57a0743566dbf6f286840d0e784cb
mark <mark@dumay.nl>
2020-11-12 07:17:31 +0100

    Enabled hot reloading of templates and certificates

--- config/nginx/docker-entrypoint.sh
+++ config/nginx/docker-entrypoint.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
-# vim:sw=4:ts=4:et
+#======================================================================================================================
+# Title         : docker-entrypoint.sh
+# Description   : Launches Nginx using server configuration templates. The execution starts once certificates are 
+#                 available. Nginx is reloaded automatically on updated templates or certificates.
+# Author        : Mark Dumay
+# Date          : November 11th, 2020
+# Version       : 0.5
+# Usage         : docker-entrypoint.sh
+# Repository    : https://github.com/markdumay/nginx-certbot
+# Comments      : 
+#======================================================================================================================
 
-# configure logging redirection
-set -e
+#======================================================================================================================
+# Constants
+#======================================================================================================================
+readonly CERT_PATH="/etc/certbot/live/${CERTBOT_DOMAIN}"
+readonly ENTRYPOINT_PATH='/docker-entrypoint.d'
+readonly NGINX_CONF_DIR='/etc/nginx/conf.d'
+readonly NGINX_SNIPPETS_DIR='/etc/nginx/snippets'
+readonly NGINX_TEMPLATES_DIR='/etc/nginx/templates'
+readonly NGINX_TEMPLATE_CMD='/docker-entrypoint.d/20-envsubst-on-templates.sh'
+# readonly POLLING_INTERVAL=60  # seconds
+readonly POLLING_INTERVAL=5  # TODO: temp
 
-if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-    exec 3>&1
-else
-    exec 3>/dev/null
-fi
+#======================================================================================================================
+# Launches any initial entrypoints scripts available in '/docker-entrypoint.d/' (source code is copied from the Nginx 
+# Docker repository). One of the default scripts ('20-envsubst-on-templates.sh') generates server configurations based
+# on templates available in the '/etc/nginx/templates' folder. Any other defined entrypoints scripts are executed too.
+#======================================================================================================================
+# Outputs:
+#   Generated server configurations in '/etc/nginx/conf.d', plus output from additionally defined entrypoint scripts.
+#======================================================================================================================
+launch_entrypoint_scripts() {
+    if find "${ENTRYPOINT_PATH}/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read -r v; then
+        echo >&3 "$0: ${ENTRYPOINT_PATH}/ is not empty, will attempt to perform configuration"
 
-# launch any initial entrypoint scripts (copied from nginx repository)
-if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
-    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read -r v; then
-        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
-
-        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
+        echo >&3 "$0: Looking for shell scripts in ${ENTRYPOINT_PATH}/"
+        find "${ENTRYPOINT_PATH}/" -follow -type f -print | sort -n | while read -r f; do
             case "$f" in
                 *.sh)
                     if [ -x "$f" ]; then
@@ -35,22 +55,98 @@ if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
 
         echo >&3 "$0: Configuration complete; ready for start up"
     else
-        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+        echo >&3 "$0: No files found in ${ENTRYPOINT_PATH}/, skipping configuration"
     fi
-fi
+}
 
-# wait for certificates to become available
-readonly CERT_PATH="/etc/certbot/live/${CERTBOT_DOMAIN}"
-while [ ! -f "${CERT_PATH}/fullchain.pem" ] || [ ! -f "${CERT_PATH}/privkey.pem" ]
-do
-    echo >&3 "$0: Waiting for certificates ('${CERT_PATH}')"
-    sleep 30
-done
+#======================================================================================================================
+# Watches the folders '/etc/nginx/templates', '/etc/nginx/templates/snippets', and 
+# '/etc/certbot/live/${CERTBOT_DOMAIN}' for any changes. Observed files should have a '.template' or '.conf' suffix.
+# In case a template has been changed, has been added, or has been removed, existing server configurations are removed 
+# entirely and recreated. Nginx is reloaded once the templates have been processed, or when a modified certificate is 
+# detected. The polling interval is set to one minute.
+#======================================================================================================================
+# Outputs:
+#   Updated server configurations in '/etc/nginx/conf.d' and snippets in '/etc/nginx/snippets'; reloaded Nginx process.
+#======================================================================================================================
+reload_nginx_on_change() {
+    prev_incoming_checksum=$(find -L "${NGINX_TEMPLATES_DIR}" -type f \( -iname \*.template -o -iname \*.conf \) \
+        -exec md5sum {} \; -maxdepth 2 2>/dev/null | sort) # covers templates and snippets
+    prev_cert_checksum=$(find -L "${CERT_PATH}"/*.pem -type f -exec md5sum {} \; -maxdepth 1 2>/dev/null | sort)
+    while true
+    do 
+        sleep "${POLLING_INTERVAL}"
 
-# start nginx in foreground and reload every 6 hours to renew any cerfificates
-"$1" -g 'daemon off;'
-while true
-do 
-    sleep 6h
-    "$1" -s reload
-done
+        # scan for any new templates or certificates
+        current_config=$(cd "${NGINX_CONF_DIR}" && find -L ./*.conf -type f -maxdepth 1 2>/dev/null | sort)
+        current_snippets=$(cd "${NGINX_SNIPPETS_DIR}" && find -L ./*.conf -type f -maxdepth 1 2>/dev/null | sort)
+        if [ -d "${NGINX_TEMPLATES_DIR}"/snippets ]; then
+            snippet_config=$(cd "${NGINX_TEMPLATES_DIR}"/snippets && find -L ./*.conf -type f -maxdepth 1 2>/dev/null | \
+                sort)
+        else
+            snippet_config=''
+        fi
+        template_config=$(cd "${NGINX_TEMPLATES_DIR}" && find -L ./*.template -type f -maxdepth 1 2>/dev/null | \
+            sed -e "s/.template//" | sort)
+        new_incoming_checksum=$(find -L "${NGINX_TEMPLATES_DIR}" -type f \( -iname \*.template -o -iname \*.conf \) \
+            -exec md5sum {} \; -maxdepth 2 2>/dev/null | sort)
+        new_cert_checksum=$(find -L "${CERT_PATH}"/*.pem -type f -exec md5sum {} \; -maxdepth 1 2>/dev/null | sort)
+
+        # generate new configuration on any new/changed templates and reload nginx
+        if  [ "${current_config}" != "${template_config}" ] || \
+            [ "${current_snippets}" != "${snippet_config}" ] || \
+            [ "${new_incoming_checksum}" != "${prev_incoming_checksum}" ]; then
+            
+            echo >&3 "$0: Changes detected, reconfiguring sites"
+            rm -rf "${NGINX_CONF_DIR}"/*.conf "${NGINX_SNIPPETS_DIR}"/*.conf || true # remove existing configurations
+            \cp -r "${NGINX_TEMPLATES_DIR}"/snippets/*.conf "${NGINX_SNIPPETS_DIR}"/ # copy incoming snippets
+            "${NGINX_TEMPLATE_CMD}" # regenerate configurations based on templates
+            prev_incoming_checksum="${new_incoming_checksum}"
+            "$1" -t && "$1" -s reload
+        # reload nginx to use renewed cerfificates
+        elif [ "${new_cert_checksum}" != "${prev_cert_checksum}" ]; then
+            prev_cert_checksum="${new_cert_checksum}"
+            "$1" -t && "$1" -s reload
+        fi
+    done    
+}
+
+#======================================================================================================================
+# Waits for certificates in the folder '/etc/certbot/live/${CERTBOT_DOMAIN}' to become available. This prevents 
+# starting Nginx prematurely. The polling interval is set to one minute.
+#======================================================================================================================
+# Outputs:
+#   Paused script execution until certificates are available.
+#======================================================================================================================
+wait_for_certificates() {
+    while [ ! -f "${CERT_PATH}/fullchain.pem" ] || [ ! -f "${CERT_PATH}/privkey.pem" ]
+    do
+        echo >&3 "$0: Waiting for certificates ('${CERT_PATH}')"
+        sleep "${POLLING_INTERVAL}"
+    done
+}
+
+#======================================================================================================================
+# Entrypoint for the script.
+#======================================================================================================================
+main() {
+    if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
+        # configure logging redirection
+        set -e
+        if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+            exec 3>&1
+        else
+            exec 3>/dev/null
+        fi
+
+        # setup initial nginx configuration, including certificates
+        rm -f "${NGINX_CONF_DIR}"/*.conf || true # remove any existing configurations at launch
+        launch_entrypoint_scripts "$@"
+        wait_for_certificates "$@"
+
+        # start nginx in foreground and scan for any configuration changes
+        "$1" -g 'daemon off;' & echo >&3 "$0: Started nginx" & reload_nginx_on_change "$@"
+    fi
+}
+
+main "$@"
