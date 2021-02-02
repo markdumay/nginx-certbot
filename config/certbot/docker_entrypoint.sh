4dfd9d589e53f4a2a708f223f86ccb9f86652f7e
mark <mark@dumay.nl>
2021-02-02 07:34:23 +0100

    Removed unncessary id command

--- config/certbot/docker_entrypoint.sh
+++ config/certbot/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run certbot script and repeat every 12 hours
-trap : TERM INT; id; certbot_issue.sh init; (while true; do certbot_issue.sh run; sleep 12h; done) & wait
+trap : TERM INT; certbot_issue.sh init; (while true; do certbot_issue.sh run; sleep 12h; done) & wait
